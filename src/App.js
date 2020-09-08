96d3169dd01fa5c7f6258547cf6fd45579021bec
Armando Rodriguez <arod1207@me.com>
2020-09-07 21:41:28 -0500

    added a footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar/NavBar';
 import Home from './components/Home/Home';
 import Projects from './components/Projects/Projects';
 import ContactMe from './components/ContactMe/ContactMe';
+import Footer from './components/Footer/Footer';
 
 function App() {
     return (
@@ -19,6 +20,7 @@ function App() {
             <Route path="/contact" exact>
                 <ContactMe />
             </Route>
+            <Footer />
         </Router>
     );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>Armando Rodriguez Jr. 2020</p>
+        </div>
+    );
+}
+
+export default Footer;
